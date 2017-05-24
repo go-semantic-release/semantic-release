0085ad3532f6d35a43bbc8c8103f2327196f6628
Christoph Witzko <github@christophwitzko.com>
2017-05-24 03:45:59 +0200

    feat(condition): support multiple CIs

--- condition/condition.go
+++ condition/condition.go
@@ -1,10 +1,22 @@
 package condition
 
 import (
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
+func readGitHead() string {
+	data, err := ioutil.ReadFile(".git/HEAD")
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSpace(strings.TrimPrefix(string(data), "ref: refs/heads/"))
+}
+
 func GetCurrentBranch() string {
-	// TODO: support other CIs
-	return os.Getenv("TRAVIS_BRANCH")
+	if val := os.Getenv("TRAVIS_BRANCH"); val != "" {
+		return val
+	}
+	return readGitHead()
 }
