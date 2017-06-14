f5f29132bb84b80a36c33c2d5fc0f851a8516e07
Joshua Chaitin-Pollak <josh@offthehill.org>
2017-06-13 20:58:13 -0400

    fix: don't write npmrc

--- update/package_json.go
+++ update/package_json.go
@@ -2,12 +2,8 @@ package update
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"os"
-	"path"
 )
-
-const npmrc = "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n"
 
 func init() {
 	Register("package.json", packageJson)
@@ -26,5 +22,6 @@ func packageJson(newVersion string, file *os.File) error {
 	if err := enc.Encode(data); err != nil {
 		return err
 	}
-	return ioutil.WriteFile(path.Join(path.Dir(file.Name()), ".npmrc"), []byte(npmrc), 0644)
+
+	return nil
 }
