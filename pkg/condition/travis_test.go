82c464c4d12a7ce0fd3ddd8ba34dfdc2139e9678
Christoph Witzko <github@christophwitzko.com>
2020-04-27 09:30:45 +0200

    test: prevent ci variables from leaking into the test

--- pkg/condition/travis_test.go
+++ pkg/condition/travis_test.go
@@ -1,6 +1,7 @@
 package condition
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,7 @@ import (
 
 func TestTravisValid(t *testing.T) {
 	travis := TravisCI{}
+	os.Setenv("TRAVIS", "")
 	err := travis.RunCondition(CIConfig{"token": "", "defaultBranch": "", "private": false})
 	assert.EqualError(t, err, "semantic-release didn’t run on Travis CI and therefore a new version won’t be published.")
 }
