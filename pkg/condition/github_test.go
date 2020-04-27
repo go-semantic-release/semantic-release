597812c3b451ad4548702bcabd112acbeb7e8118
Christoph Witzko <github@christophwitzko.com>
2020-04-27 09:30:45 +0200

    test: prevent ci variables from leaking into the test

--- pkg/condition/github_test.go
+++ pkg/condition/github_test.go
@@ -1,6 +1,7 @@
 package condition
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,7 @@ import (
 
 func TestGithubValid(t *testing.T) {
 	gha := GitHubActions{}
+	os.Setenv("GITHUB_REF", "")
 	err := gha.RunCondition(CIConfig{"defaultBranch": ""})
 	assert.EqualError(t, err, "This test run is not running on a branch build.")
 }
--- pkg/condition/gitlab_test.go
+++ pkg/condition/gitlab_test.go
@@ -1,6 +1,7 @@
 package condition
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,7 @@ import (
 
 func TestGitlabValid(t *testing.T) {
 	gl := GitLab{}
+	os.Setenv("CI_COMMIT_BRANCH", "")
 	err := gl.RunCondition(CIConfig{"defaultBranch": ""})
 	assert.EqualError(t, err, "This test run is not running on a branch build.")
 }
