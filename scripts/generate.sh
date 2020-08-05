2cc56cf6ad12f1488093f4877c7159347e56bd0e
Christoph Witzko <github@christophwitzko.com>
2020-08-05 12:07:04 +0200

    chore: impreove generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-protoc -I ./ ./pkg/semrel/*.proto ./pkg/provider/*.proto --go_opt=paths=source_relative --go_out=./
-protoc -I ./ ./pkg/plugin/*.proto --go_opt=paths=source_relative --go_out=./ --go-grpc_opt=paths=source_relative --go-grpc_out=./
+shopt -s globstar
+
+for f in $(ls ./pkg/**/*.proto) ; do
+  echo "generating $f"
+  protoc -I ./ $f --go_opt=paths=source_relative --go_out=./ --go-grpc_opt=paths=source_relative --go-grpc_out=./
+done
