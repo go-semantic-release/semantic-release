d0fe7d98fc2289e5d5e95b2611dfad47d42ec4ee
Christoph Witzko <github@christophwitzko.com>
2020-08-10 10:56:33 +0200

    feat: add ci condition grpc interface

---
+++ pkg/condition/ci_condition_wrapper.go
@@ -0,0 +1,73 @@
+package condition
+
+import (
+	"context"
+	"errors"
+)
+
+const CIConditionPluginName = "ci_condition"
+
+type CIConditionServer struct {
+	Impl CICondition
+	UnimplementedCIConditionPluginServer
+}
+
+func (c CIConditionServer) Name(ctx context.Context, request *Name_Request) (*Name_Response, error) {
+	return &Name_Response{Value: c.Impl.Name()}, nil
+}
+
+func (c CIConditionServer) RunCondition(ctx context.Context, request *RunCondition_Request) (*RunCondition_Response, error) {
+	err := c.Impl.RunCondition(request.Value)
+	ret := &RunCondition_Response{}
+	if err != nil {
+		ret.Error = err.Error()
+	}
+	return ret, nil
+}
+
+func (c CIConditionServer) GetCurrentBranch(ctx context.Context, request *GetCurrentBranch_Request) (*GetCurrentBranch_Response, error) {
+	return &GetCurrentBranch_Response{Value: c.Impl.GetCurrentBranch()}, nil
+}
+
+func (c CIConditionServer) GetCurrentSHA(ctx context.Context, request *GetCurrentSHA_Request) (*GetCurrentSHA_Response, error) {
+	return &GetCurrentSHA_Response{Value: c.Impl.GetCurrentSHA()}, nil
+}
+
+type CIConditionClient struct {
+	Impl CIConditionPluginClient
+}
+
+func (c CIConditionClient) Name() string {
+	name, err := c.Impl.Name(context.Background(), &Name_Request{})
+	if err != nil {
+		panic(err)
+	}
+	return name.Value
+}
+
+func (c CIConditionClient) RunCondition(m map[string]string) error {
+	res, err := c.Impl.RunCondition(context.Background(), &RunCondition_Request{Value: m})
+	if err != nil {
+		return err
+	}
+	if res.Error == "" {
+		return nil
+	}
+	return errors.New(res.Error)
+}
+
+func (c CIConditionClient) GetCurrentBranch() string {
+	res, err := c.Impl.GetCurrentBranch(context.Background(), &GetCurrentBranch_Request{})
+	if err != nil {
+		panic(err)
+	}
+	return res.Value
+}
+
+func (c CIConditionClient) GetCurrentSHA() string {
+	res, err := c.Impl.GetCurrentSHA(context.Background(), &GetCurrentSHA_Request{})
+	if err != nil {
+		panic(err)
+	}
+	return res.Value
+}
