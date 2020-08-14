c28397f6e42d2023671cd487b91992b4f7e09fae
Christoph Witzko <github@christophwitzko.com>
2020-08-14 14:02:03 +0200

    refactor: update config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,33 +8,26 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-type (
-	// Config is a complete set of app configuration
-	Config struct {
-		Token                           string
-		Slug                            string
-		Changelog                       string
-		Ghr                             bool
-		Noci                            bool
-		Dry                             bool
-		Vf                              bool
-		Update                          string
-		GheHost                         string
-		Prerelease                      bool
-		TravisCom                       bool
-		BetaRelease                     *BetaRelease
-		Match                           string
-		AllowInitialDevelopmentVersions bool
-		AllowNoChanges                  bool
-		GitLab                          bool
-		GitLabBaseURL                   string
-		GitLabProjectID                 string
-	}
-
-	BetaRelease struct {
-		MaintainedVersion string `json:"maintainedVersion"`
-	}
-)
+// Config is a complete set of app configuration
+type Config struct {
+	Token                           string
+	Slug                            string
+	Changelog                       string
+	Ghr                             bool
+	Noci                            bool
+	Dry                             bool
+	Vf                              bool
+	Update                          string
+	GheHost                         string
+	Prerelease                      bool
+	Match                           string
+	AllowInitialDevelopmentVersions bool
+	AllowNoChanges                  bool
+	GitLab                          bool
+	GitLabBaseURL                   string
+	GitLabProjectID                 string
+	MaintainedVersion               string
+}
 
 // NewConfig returns a new Config instance
 func NewConfig(c *cli.Context) (*Config, error) {
@@ -49,14 +42,13 @@ func NewConfig(c *cli.Context) (*Config, error) {
 		Update:                          c.String("update"),
 		GheHost:                         c.String("ghe-host"),
 		Prerelease:                      c.Bool("prerelease"),
-		TravisCom:                       c.Bool("travis-com"),
 		Match:                           c.String("match"),
 		AllowInitialDevelopmentVersions: c.Bool("allow-initial-development-versions"),
 		AllowNoChanges:                  c.Bool("allow-no-changes"),
 		GitLab:                          c.Bool("gitlab"),
 		GitLabBaseURL:                   c.String("gitlab-base-url"),
 		GitLabProjectID:                 c.String("gitlab-project-id"),
-		BetaRelease:                     &BetaRelease{},
+		MaintainedVersion:               "",
 	}
 
 	f, err := os.OpenFile(".semrelrc", os.O_RDONLY, 0)
@@ -65,9 +57,12 @@ func NewConfig(c *cli.Context) (*Config, error) {
 	}
 	defer f.Close()
 
-	if err := json.NewDecoder(f).Decode(conf.BetaRelease); err != nil {
+	var configFile map[string]string
+	if err := json.NewDecoder(f).Decode(&configFile); err != nil {
 		return nil, fmt.Errorf("could not parse .semrelrc: %w", err)
 	}
+
+	conf.MaintainedVersion = configFile["maintainedVersion"]
 
 	return conf, nil
 }
