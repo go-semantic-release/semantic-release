fbbc3704d1de9d9272abafacbe05ad647dc19006
Christoph Witzko <github@christophwitzko.com>
2020-10-28 12:16:46 +0100

    chore: update generate script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,11 +2,7 @@
 
 set -euo pipefail
 
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  shopt -s globstar
-fi
-
-for f in $(ls ./pkg/**/*.proto) ; do
+for f in $(find ./pkg -name "*.proto") ; do
   echo "generating $f"
   protoc -I ./ $f --go_opt=paths=source_relative --go_out=./ --go-grpc_opt=paths=source_relative --go-grpc_out=./
 done
