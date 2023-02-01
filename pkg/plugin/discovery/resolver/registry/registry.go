9e6e36efa3b9254662e9571a23b1724ee73b1479
Christoph Witzko <github@christophwitzko.com>
2023-02-01 22:41:12 +0100

    feat(registry): add single plugin fetching

--- pkg/plugin/discovery/resolver/registry/registry.go
+++ pkg/plugin/discovery/resolver/registry/registry.go
@@ -1,8 +1,12 @@
 package registry
 
 import (
+	"context"
 	"fmt"
+	"runtime"
+	"sort"
 
+	"github.com/Masterminds/semver/v3"
 	"github.com/go-semantic-release/plugin-registry/pkg/client"
 	"github.com/go-semantic-release/semantic-release/v2/pkg/plugin"
 	"github.com/go-semantic-release/semantic-release/v2/pkg/plugin/discovery/resolver"
@@ -21,6 +25,63 @@ func NewResolver() *Resolver {
 }
 
 func (r *Resolver) ResolvePlugin(pluginInfo *plugin.Info) (*resolver.PluginDownloadInfo, error) {
+	getPluginRes, err := r.client.GetPlugin(context.Background(), pluginInfo.ShortNormalizedName)
+	if err != nil {
+		return nil, err
+	}
+
+	osArch := fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+	if pluginInfo.Constraint == nil {
+		foundAsset := getPluginRes.LatestRelease.Assets[osArch]
+		if foundAsset == nil {
+			return nil, fmt.Errorf("a matching plugin was not found for %s/%s", runtime.GOOS, runtime.GOARCH)
+		}
+		return &resolver.PluginDownloadInfo{
+			URL:      foundAsset.URL,
+			Checksum: foundAsset.Checksum,
+			FileName: foundAsset.FileName,
+			Version:  getPluginRes.LatestRelease.Version,
+		}, nil
+	}
+
+	foundVersion := ""
+	versions := make(semver.Collection, 0)
+	for _, v := range getPluginRes.Versions {
+		pv, sErr := semver.NewVersion(v)
+		if sErr != nil {
+			return nil, sErr
+		}
+		versions = append(versions, pv)
+	}
+	sort.Sort(sort.Reverse(versions))
+	for _, v := range versions {
+		if pluginInfo.Constraint.Check(v) {
+			foundVersion = v.String()
+			break
+		}
+	}
+	if foundVersion == "" {
+		return nil, fmt.Errorf("no matching version was found")
+	}
+
+	pluginRelease, err := r.client.GetPluginRelease(context.Background(), pluginInfo.ShortNormalizedName, foundVersion)
+	if err != nil {
+		return nil, err
+	}
+	foundAsset := pluginRelease.Assets[osArch]
+	if foundAsset == nil {
+		return nil, fmt.Errorf("a matching plugin was not found for %s/%s", runtime.GOOS, runtime.GOARCH)
+	}
+
+	return &resolver.PluginDownloadInfo{
+		URL:      foundAsset.URL,
+		Checksum: foundAsset.Checksum,
+		FileName: foundAsset.FileName,
+		Version:  getPluginRes.LatestRelease.Version,
+	}, nil
+}
+
+func (r *Resolver) BatchResolvePlugins(pluginInfos []*plugin.Info) (*resolver.BatchPluginDownloadInfo, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
